425d128ffb225ba039dc5f93230af5a32017e0db
Shittu-kumar <shittukumar37@gmail.com>
2025-08-08 16:13:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -822,10 +822,10 @@ async def txt_handler(bot: Client, m: Message):
                         print("All signing attempts failed, trying last received URL anyway...")
 
             elif "childId" in url and "parentId" in url:
-                url = f"https://anonymouspwplayerr-f996115ea61a.herokuapp.com/pw?url={url}&token={raw_text4}"
+                url = f"https://anonymouspwplayer-0e5a3f512dec.herokuapp.com/pw?url={url}&token={raw_text4}"
                            
             elif "d1d34p8vz63oiq" in url or "sec1.pw.live" in url:
-                url = f"https://anonymouspwplayerr-f996115ea61a.herokuapp.com/pw?url={url}&token={raw_text4}"
+                url = f"https://anonymouspwplayer-0e5a3f512dec.herokuapp.com/pw?url={url}&token={raw_text4}"
 
             if ".pdf*" in url:
                 url = f"https://dragoapi.vercel.app/pdf/{url}"
